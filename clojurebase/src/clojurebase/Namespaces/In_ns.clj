a1713776e5c8095af418a25a7cd07255983dfd99
liyikun <liyikunl@live.com>
2017-12-17 21:52:19 +0800

    练习

---
+++ clojurebase/src/clojurebase/Namespaces/In_ns.clj
@@ -0,0 +1,10 @@
+(ns clojurebase.Namespaces.In-ns)
+
+(defn favorite-language [] "Clojure!!")
+
+(favorite-language)
+
+
+(in-ns 'user)
+
+(favorite-language)
---
+++ clojurebase/src/clojurebase/Namespaces/Require.clj
@@ -0,0 +1,1 @@
+(ns clojurebase.Namespaces.Require)
