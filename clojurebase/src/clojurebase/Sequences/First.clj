addda379caa3c6baafe3881c42fdad2e59354b40
liyikun <liyikunl@live.com>
2018-02-03 20:42:06 +0800

    get

---
+++ clojurebase/src/clojurebase/Sequences/First.clj
@@ -0,0 +1,9 @@
+(ns clojurebase.Sequences.First)
+
+(first [1 2 3])
+
+(first #{3 2 1})
+
+(first '(1 2 3))
+
+(first "abc")
---
+++ clojurebase/src/clojurebase/Sequences/Rest.clj
@@ -0,0 +1,7 @@
+(ns clojurebase.Sequences.Rest)
+
+(rest [1 2 3])
+
+(type [1 2 3])
+
+(type (rest [1 2 3]))
